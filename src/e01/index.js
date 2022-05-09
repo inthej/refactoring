0c9255fdea528ea60fa6d409840fad7fd33d4a46
inthej <inthej12@gmail.com>
2022-05-09 22:47:26 +0900

    Renaming And Extract Play function

--- src/e01/index.js
+++ src/e01/index.js
@@ -8,7 +8,7 @@ function statement(invoice, plays) {
   }).format
   
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID]
+    const play = playFor(perf)
     const thisAmount = amountFor(perf, play)
 
     // 포인트를 적립한다
@@ -24,28 +24,32 @@ function statement(invoice, plays) {
   result += `총액: ${format(totalAmount / 100)}\n`
   result += `적립 포인트: ${volumeCredits}점\n`
   return result
-}
 
-function amountFor(perf, play) {
-  let thisAmount = 0
-  switch (play.type) {
-    case "tragedy": // 비극
-      thisAmount = 40000
-      if (perf.audience > 30) {
-        thisAmount += 1000 * (perf.audience - 30)
-      }
-      break;
-    case "comedy": // 희극
-      thisAmount = 30000
-      if (perf.audience > 20) {
-        thisAmount += 10000 + 500 * (perf.audience - 20)
-      }
-      thisAmount += 300 * perf.audience
-      break;
-    default:
-      throw new Error(`알 수 없는 장르: ${play.type}`)
+  function amountFor(aPerformance, play) {
+    let result = 0
+    switch (play.type) {
+      case "tragedy": // 비극
+        result = 40000
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30)
+        }
+        break;
+      case "comedy": // 희극
+        result = 30000
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20)
+        }
+        result += 300 * aPerformance.audience
+        break;
+      default:
+        throw new Error(`알 수 없는 장르: ${play.type}`)
+    }
+    return result
   }
-  return thisAmount
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID]
+  }
 }
 
 export { statement }
