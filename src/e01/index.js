7782fb574cd556f36a997d14ded98d1b99972650
inthej <inthej12@gmail.com>
2022-05-09 23:00:33 +0900

    Inline Play and Amount

--- src/e01/index.js
+++ src/e01/index.js
@@ -8,26 +8,23 @@ function statement(invoice, plays) {
   }).format
   
   for (let perf of invoice.performances) {
-    const play = playFor(perf)
-    const thisAmount = amountFor(perf, play)
-
     // 포인트를 적립한다
     volumeCredits += Math.max(perf.audience - 30, 0)
 
     // 희극 관객 5명마다 추가 포인트를 제공한다
-    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5)
+    if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5)
 
     // 청구 내역을 출력한다
-    result += ` ${play.name}: ${format(thisAmount/100)} (${perf.audience}석)\n`
-    totalAmount += thisAmount
+    result += ` ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience}석)\n`
+    totalAmount += amountFor(perf)
   }
   result += `총액: ${format(totalAmount / 100)}\n`
   result += `적립 포인트: ${volumeCredits}점\n`
   return result
 
-  function amountFor(aPerformance, play) {
+  function amountFor(aPerformance) {
     let result = 0
-    switch (play.type) {
+    switch (playFor(aPerformance).type) {
       case "tragedy": // 비극
         result = 40000
         if (aPerformance.audience > 30) {
